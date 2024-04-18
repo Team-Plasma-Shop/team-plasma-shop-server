f4c44e4653b80cb2dba157e6e62b2b715661af22
Smachy <pro.diegocdossantos@gmail.com>
2024-04-18 16:38:37 +0200

    fix password user hash

--- src/EventSubscriber/PasswordSubscriber.php
+++ src/EventSubscriber/PasswordSubscriber.php
@@ -4,30 +4,27 @@ namespace App\EventSubscriber;
 
 use ApiPlatform\Symfony\EventListener\EventPriorities;
 use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request; 
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class PasswordSubscriber implements EventSubscriberInterface
+#[AsDoctrineListener(event: Events::prePersist)]
+class PasswordSubscriber
 {
     public function __construct(private UserPasswordHasherInterface $userPasswordHasherInterface){
 ;
     }
-
-    public function hashPassword(ViewEvent $event): void
-    {
-        var_dump($event);
-        $entity = $event->getControllerResult();
-        $method = $event->getRequest()->getMethod();
-
-        if(!$entity instanceof User || $method !== Request::METHOD_POST){
+    public function prePersist(LifecycleEventArgs $lifecycleEventArgs): void{
+        $user = $lifecycleEventArgs->getObject();
+        if(!$user instanceof User){
             return;
         }
-
-        $hashedPassword = $this->userPasswordHasherInterface->hashPassword($entity, $entity->getPassword());
-        $entity->setPassword($hashedPassword);
+        $user->setPassword($this->userPasswordHasherInterface->hashPassword($user, $user->getPassword()));
     }
 
     public static function getSubscribedEvents(): array
